671dc92b8c87c768487ec427cadf7cc188e4e1cf
yousef-hussain <yousef123hussain@gmail.com>
2023-03-08 09:51:34 +0200

    return to last path

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/profile', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
 
 
-Route::get('/', function () {
+Route::get('/profile', function () {
     return view('profile');
 });
 
